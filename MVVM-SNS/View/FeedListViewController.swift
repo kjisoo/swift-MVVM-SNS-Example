ae15d87c42b7a08f7926782516887ce9697ed197
kjisoo <kim@jisoo.net>
2018-10-06 16:01:19 +0900

    Add Feed view as empty

---
+++ MVVM-SNS/View/FeedListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FeedListViewController.swift
+//  MVVM-SNS
+//
+//  Created by A on 06/10/2018.
+//  Copyright © 2018 Kimjisoo. All rights reserved.
+//
+
+import UIKit
+
+class FeedListViewController: BaseViewController {
+}
